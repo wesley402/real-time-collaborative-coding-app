590b01b629958a5a83f592fe20bcb29d296e65fa
Wesley <mr.chung402@gmail.com>
2019-03-22 17:47:07 -0400

    bug fix

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,13 @@
 fuser -k 3000/tcp
 service redis_6379 start
+
 cd ./server
 nodemon server.js &
-cd ../client
-ng build --watch &
+
+
+cd ../executor
+python3 executor_server.py &
+
 
 echo "=================================================="
 read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
